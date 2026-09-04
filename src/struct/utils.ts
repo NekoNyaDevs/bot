b76b19c480bca2b2ea0af86423cf6e8955ec06d9
Adam <contact@classydev.fr>
2026-09-04 16:49:45 +0200

    fix: paths

--- src/struct/utils.ts
+++ src/struct/utils.ts
@@ -1,3 +1,5 @@
 import path from "node:path";
 
-export const root = path.join(process.cwd(), 'src');
+const isDev = process.argv[1].endsWith('.ts');
+const baseDir = isDev ? 'src' : 'lib';
+export const root = path.join(process.cwd(), baseDir);
